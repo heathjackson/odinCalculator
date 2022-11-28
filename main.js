02e2ab9e84c44c450b56293761615021ee3eb482
Heather Jackson <heather.jackson2003@gmail.com>
2022-11-28 17:13:41 -0500

    basic css written

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
+
+// ****** basic math functions *************/
+
 function add (a, b) {
   return a + b;
 }
-
-console.log(add(5, 6));
 
 function subtract (a, b) {
   return a - b;
 }
 
-console.log(subtract(5, 6));
-
 function multiply (a, b) {
   return a * b;
 }
-
-console.log(multiply(5, 6));
 
 function divide (a, b) {
   return a / b;
 }
 
-console.log(divide(12, 6));
+//************ operator function **************/
 
 function operate (operator, a, b) {
   return operator(a, b);
 }
 
-console.log(operate (subtract, 10, 5));
+console.log(operate(divide, 10, 5))
+
+
+
+
