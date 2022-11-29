093a5cb69d37260817d9a9786ef9fc5bacbe9e61
Heather Jackson <heather.jackson2003@gmail.com>
2022-11-28 23:49:12 -0500

    attempting calculator with arrays

--- main.js
+++ main.js
@@ -26,5 +26,29 @@ function operate (operator, a, b) {
 console.log(operate(divide, 10, 5))
 
 
+let numButton = document.querySelectorAll('.number');
+
+let numArray1 = [];
+let array1Concacted = [];
+let numArray2 = [];
+let array2Concacted = [];
+
+
+for (const numBtn of numButton) {
+  numBtn.addEventListener('click', () => {
+    num = numBtn.textContent;
+    numArray1.push(num);
+    let numArrayJoin = +(numArray1.join(''));
+    document.getElementById('display')
+    .innerHTML = numArrayJoin; 
+    array1Concacted.push(numArrayJoin);  
+  });
+}
+
+console.log (array1Concacted);
+
+
+ 
+
 
 
