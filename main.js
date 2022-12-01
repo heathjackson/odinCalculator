f28848a5936c39d4354f011243e5165cae83b56e
Heather Jackson <heather.jackson2003@gmail.com>
2022-12-01 01:51:07 -0500

    calculator fully functioning - bug after push = and then number does not clear

--- main.js
+++ main.js
@@ -1,4 +1,59 @@
 
+
+let number = document.querySelectorAll('.number');
+let opera = document.querySelectorAll('.opera');
+const equal = document.getElementById('equals');
+const clear = document.getElementById('clear');
+const display = document.getElementById('display')
+const decimal = document.getElementById('decimal')
+
+let numArray = [];
+let joinedArray1 = [];
+let joinedArray2 = [];
+let op = '';
+
+decimal.addEventListener('click', () => {
+  if (numArray.length === 0 || numArray.includes('.') == false) {
+    numArray.push('.');
+    console.log(numArray);
+  }
+  else {
+    decimal.removeEventListener;
+  } 
+});
+
+for (const numBtn of number) {
+  numBtn.addEventListener('click', () => {
+    num = numBtn.textContent;
+    numArray.push(num);
+    arrayCombined();
+    console.log({joinedArray2});
+    console.log({joinedArray1});
+  });
+}
+
+for (const ope of opera) {
+  ope.addEventListener('click', () => {
+    operators(ope);
+  });
+}
+
+//makes equal button work
+
+equal.addEventListener('click', () => {
+  let total = operate(op, joinedArray1, joinedArray2);
+  display.innerHTML = total;
+});
+
+//clears all the arrays
+
+clear.addEventListener('click', () => {
+  numArray = [];
+  joinedArray1 = [];
+  joinedArray2 = [];
+  display.innerHTML = 0;
+});
+
 // ****** basic math functions *************/
 
 function add (a, b) {
@@ -23,72 +78,45 @@ function operate (operator, a, b) {
   return operator(a, b);
 }
 
-let number = document.querySelectorAll('.number');
-let opera = document.querySelectorAll('.opera');
-const equal = document.getElementById('equals');
-
-let numArray = [];
-let joinedArray1 = [];
-let joinedArray2 = [];
-
-let op = ''; 
-
-for (const numBtn of number) {
-  numBtn.addEventListener('click', () => {
-    num = numBtn.textContent;
-    numArray.push(num);
-    arrayCombined();
-    console.log({joinedArray2});
-    console.log({joinedArray1});
-  });
-}
-
-for (const ope of opera) {
-  ope.addEventListener('click', () => {
-    operators(ope);
-  });
-}
-
-function operators(ope) {
-    if (joinedArray1.length !== 0) {
-      joinedArray2 = operate(op, joinedArray1, joinedArray2);
-    }
-
-    console.log({joinedArray1});
-    joinedArray1 = joinedArray2;
-    joinedArray2 = [];
-    numArray = [];
-
-    op = ope.textContent;
-    console.log(op);
-    if (op === '+') {
-      op = add;
-    }
-    if (op === '-') {
-      op = subtract;
-    }
-
-    if (op === 'x') {
-      op = multiply;
-    }
-
-    if (op === '/') {
-      op = divide;
-    } 
-    console.log(op);
-}
-
-equal.addEventListener('click', () => {
-  console.log(operate(op, joinedArray1, joinedArray2))
-});
-
 //function to combine an array of strings into one number 
 
 function arrayCombined() {
     let numArrayJoined = +(numArray.join(''));
-    document.getElementById('display')
-    .innerHTML = numArrayJoined;
-    joinedArray2 = numArrayJoined; 
+    rounded = Number(Math.round(numArrayJoined+'e'+10)+'e-'+10);
+    display.innerHTML = rounded;
+    joinedArray2 = rounded; 
+}
+
+//connects html operators to js operator functions
+
+function operators(ope) {
+  if (joinedArray1.length !== 0) {
+    joinedArray2 = operate(op, joinedArray1, joinedArray2);
+  }
+
+  console.log({joinedArray1});
+  joinedArray1 = joinedArray2;
+  joinedArray2 = [];
+  numArray = [];
+
+  op = ope.textContent;
+  console.log(op);
+
+  if (op === '+') {
+    op = add;
+  }
+
+  if (op === '-') {
+    op = subtract;
+  }
+
+  if (op === 'x') {
+    op = multiply;
+  }
+
+  if (op == '/') {
+    op = divide;
+  } 
 }
 
 
